4c52e8055ec6115f3ca4102388dafd4e78c92da2
musichumgama <89453330+Hackergaming7667@users.noreply.github.com>
2021-08-30 19:32:30 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,77 @@
+let allMusic = [
+ 
+{
+    name: "Lamborghini",
+    artist: "Singers:- Neha Kakkar & Jassie Gill",
+    img: "Thapa-1",
+    src: "Thapa-1",
+},
+{
+    name: "BHAI BHAI",
+    artist: "Singer:- Mika Singh",
+    img: "Thapa-2",
+    src: "Thapa-2",
+},
+{
+    name: "Barsaat Ki Dhun",
+    artist: "Singer:- Jubin Nautiyal",
+    img: "Thapa-3",
+    src: "Thapa-3",
+},
+{
+    name: "Param Sundari",
+    artist: "Singer:- Shreya Ghoshal",
+    img: "Thapa-4",
+    src: "Thapa-4",
+},
+{
+    name: "Raataan Lambiyan",
+    artist: "Singer:- Jubin Nautiyal & Asees Kaur",
+    img: "Thapa-5",
+    src: "Thapa-5",
+},
+{
+    name: "Wafa Na Raas Aayee",
+    artist: "Singer:- Meet Bros & Jubin Nautiyal",
+    img: "Thapa-6",
+    src: "Thapa-6",
+},
+{
+    name: "Leja Re",
+    artist: "Singer:-Dhvani Bhanushali",
+    img: "Thapa-7",
+    src: "Thapa-7",
+},
+{
+    name: "Ishq Mera",
+    artist: "Singer:- Pratibha Singh Baghel",
+    img: "Thapa-8",
+    src: "Thapa-8",
+},
+{
+    name: "Bhuj Theme",
+    artist: "Singer:- Shikha Joshi",
+    img: "Thapa-9",
+    src: "Thapa-9",
+},
+{
+    name: "Duniyaa",
+    artist: "Singer:- Akhil, Dhvani Bhanushali",
+    img: "Thapa-10",
+    src: "Thapa-10",
+},
+{
+    name: "Desh Mere",
+    artist: "Singer:- Arijit Singh",
+    img: "Thapa-11",
+    src: "Thapa-11",
+},
+{
+    name: "Ishq Tera",
+    artist: "Singer:- Guru Randhawa",
+    img: "Thapa-12",
+    src: "Thapa-12",
+},
+ 
+ 
+]; 
